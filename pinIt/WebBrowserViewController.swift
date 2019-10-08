c9f406c38c88bb4e2fe35cd2cf457b55bac62bc8
Dejan-Tomic <dejan.professional@gmail.com>
2019-10-08 16:10:09 -0300

    Added function that allows the user to press button '1' and open a new view with a link to stackoverflow.

--- pinIt/WebBrowserViewController.swift
+++ pinIt/WebBrowserViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class WebBrowserViewController: UIViewController {
 
+    
+    @IBOutlet weak var webBrowser: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.stackoverflow.com")!
+        
+        webBrowser.loadRequest(URLRequest(url: url))
+
+
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
