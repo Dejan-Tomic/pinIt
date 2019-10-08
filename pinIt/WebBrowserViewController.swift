5fc51a89805bbbad49665ed3de5dd08ccd237f9f
Dejan-Tomic <dejan.professional@gmail.com>
2019-10-08 15:56:57 -0300

    Created web browser viewconroller.

---
+++ pinIt/WebBrowserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WebBrowserViewController.swift
+//  pinIt
+//
+//  Created by Dejan Tomic on 08/10/2019.
+//  Copyright © 2019 Dejan Tomic. All rights reserved.
+//
+
+import UIKit
+
+class WebBrowserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
